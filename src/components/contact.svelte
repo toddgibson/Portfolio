0d35ef95a905faf2d0649fa14296cdb545ef794d
toddgibson <justtoddgibson@gmail.com>
2023-04-03 20:48:48 -0400

    update contact page

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -4,11 +4,12 @@
 </div>
 
 <div class="text-center" style="min-height: 35vh;">
-    <div class="contact-item"><strong><img src="/images/mail-logo.png" alt="email me" style="height: 20px;"></strong> <a
-            href="mailto: justtoddgibson+portfolio@gmail.com">email me</a></div>
+    <div class="contact-item"><strong><img src="/images/mail-logo.png" alt="email me" style="height: 20px;">
+        </strong> <a href="mailto: justtoddgibson+portfolio@gmail.com">email me</a>
+    </div>
     <div class="contact-item">
-        <a href="https://www.linkedin.com/in/justtoddgibson/"><img src="/images/linkedin-logo.png" alt="linked in logo"
-                style="height: 20px;"></a>&nbsp;&nbsp;
-        <a href="https://twitter.com/justtoddgibson"><img src="/images/twitter-logo.png" style="height: 20px;" alt="twitter logo"></a>
+        <a href="https://www.linkedin.com/in/justtoddgibson/" target="_contact"><img src="/images/linkedin-logo.png" alt="linked in logo" style="height: 20px;"></a>&nbsp;&nbsp;
+        <a href="https://twitter.com/justtoddgibson" target="_contact"><img src="/images/twitter-logo.png" style="height: 20px;" alt="twitter logo"></a>
+        <a href="https://github.com/toddgibson" target="_contact"><img src="/images/github-logo.png" style="height: 20px;" alt="github logo"></a>
     </div>
 </div>
